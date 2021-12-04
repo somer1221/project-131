5ed2664c587c9bd88bd853ddb14e5a99bbe27361
somer1221 <79031075+somer1221@users.noreply.github.com>
2021-12-03 21:17:27 -0500

    Add files via upload

--- living.js
+++ living.js
@@ -1,30 +1,51 @@
-imamge = "";
+image1 = "";
 statusofmodel = "";
+newresults = [];
 
 function preload() {
- //image = loadImage('');
+   image1 = loadImage('living room.jpg');
 }
 
 function setup() {
     canvas = createCanvas(640,420);
     canvas.center();
-    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Model Status: Detecting Objects"
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Model Status: Detecting Objects";
 }
 
-
-
 function modelLoaded() {
-    console.log("CocoSSD 'not sure how to spell' has been initialized");
+    console.log("Cocossd has been initialized");
     statusofmodel = true;
-    objectDetector.detect(img, gotResults);
+    
 }
 
 function gotResults(error, results) {
     if (error) {
         console.log(error);
-        console.log("Error occured.")
+        console.log("Error occured.");
     }
         console.log(results);
-        console.log("Results sent!")
+        console.log("Results sent!");
+        newresults = results;
     }
+
+function draw() {
+  image(image1, 0, 0, 640, 420);
+    
+   if(statusofmodel != "") {
+    objectDetector.detect(image1, gotResults);
+    for(i=0;i<results.length;i++) {
+        console.log("XADJSLA;JDLAKSJDLAKSJDLAJSKDJALJSLDJHAHWKDSGDSAGDHKSGJHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+     document.getElementById("status").innerHTML = "Model Status: Objects Detected";
+     fill("#FF0000");
+        percentage = floor(results[i].confidence * 100);
+        text(results[i].label + " " + percentage + "%", results[i].x, results[i].y);
+        noFill();
+        stroke("#FF0000");
+        rect(results[i].x, results[i].y, results[i].width, results[i].height);
+    }
+  }
+}
+ 
+
+
